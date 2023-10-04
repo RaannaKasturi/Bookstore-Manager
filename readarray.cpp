e4e94f65f8a3fdf8b69ef8fa3289169259e5bbb5
raannakasturi <raannakasturi@gmail.com>
2023-10-04 17:16:21 +0530

    final array read code

--- readarray.cpp
+++ readarray.cpp
@@ -3,57 +3,48 @@
 
 using namespace std;
 
-struct Book {
-    int id;
-    string name;
-    string author;
-    int price;
+string books[][4] = { //books array
+    {"1", "Book 1", "Author 1", "52"},
+    {"2", "Book 2", "Author 3", "65"},
+    {"3", "Book 3", "Author 1", "35"},
+    {"4", "Book 4", "Author 3", "33"},
+    {"5", "Book 5", "Author 2", "58"},
+    {"6", "Book 6", "Author 4", "45"},
+    {"7", "Book 7", "Author 3", "55"},
+    {"8", "Book 8", "Author 1", "12"},
+    {"9", "Book 9", "Author 5", "9"},
+    {"10", "Book 10", "Author 2", "24"}
 };
 
-Book books[] = {
-    {1, "Book 1", "Author 1", 52},
-    {2, "Book 2", "Author 3", 65},
-    {3, "Book 3", "Author 1", 35},
-    {4, "Book 4", "Author 3", 33},
-    {5, "Book 5", "Author 2", 58},
-    {6, "Book 6", "Author 4", 45},
-    {7, "Book 7", "Author 3", 55},
-    {8, "Book 8", "Author 1", 12},
-    {9, "Book 9", "Author 5", 9},
-    {10, "Book 10", "Author 2", 24}
-};
-
-int main() {
-    int BookID;
-    bool found;
-
-    while (true) { // Run the program in an infinite loop
-        found = false; // Reset the found flag for each iteration
-
-        cout << "Enter Book ID (or enter 0 to exit): ";
+void searchBook(){ //searchbook function
+    while (true) { //take user input for BookID
+        cout << "Enter 0 to exit the Program!" << endl;
+        cout << "Enter Book ID: ";
+        int BookID;
         cin >> BookID;
 
-        if (BookID == 0) {
+        if (BookID == 0) { //exiting the loop and the program
             cout << "Exiting the program." << endl;
-            break; // Exit the loop and the program if the user enters 0
+            break; 
         }
-
-        for (const Book &book : books) {
-            if (book.id == BookID) {
-                found = true;
-                cout << "Book Details:" << endl;
-                cout << "Book ID: " << book.id << endl;
-                cout << "Book Name: " << book.name << endl;
-                cout << "Book Author: " << book.author << endl;
-                cout << "Book Price: ₹" << book.price << endl;
-                break; // Exit the loop once a matching book is found
+        
+        if (BookID >= 1 && BookID <= 10) { //ranging the array search from 1 to 10
+            int arrayIndex = BookID - 1;
+            
+            cout << "Book Details:" << endl; //displaying book details
+            cout << "Book ID: " << books[arrayIndex][0] << endl;
+            cout << "Book Name: " << books[arrayIndex][1] << endl;
+            cout << "Book Author: " << books[arrayIndex][2] << endl;
+            cout << "Book Price: ₹" << books[arrayIndex][3] << endl;
+        } 
+        else {
+            cout << "Invalid Book ID. Please enter a number between 1 and 10." << endl;
             }
-        }
-
-        if (!found) {
-            cout << "Book not found." << endl;
-        }
     }
+}
 
+int main() {
+    cout << "Welcome!" << endl;
+    searchBook();
     return 0;
 }
